809f476f5de4a54d41958a66b63383549e20623e
August <augustgants@gmail.com>
2026-09-07 01:42:51 +0300

    add 2 tasks  kyu8

---
+++ src/kyu8/tasks.rs
@@ -0,0 +1,32 @@
+// Grade book
+// Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+// Numerical Score	Letter Grade
+// 90 <= score <= 100	'A'
+// 80 <= score < 90	'B'
+// 70 <= score < 80	'C'
+// 60 <= score < 70	'D'
+// 0 <= score < 60	'F'
+// Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+pub fn get_grade(s1: u16, s2: u16, s3: u16) -> char {
+    let avg = (s1 + s2 + s3) / 3;
+
+    match avg {
+        90..=100 => 'A',
+        80..=89 => 'B',
+        70..=79 => 'C',
+        60..=69 => 'D',
+        0..=59 => 'F',
+        _ => panic!("Invalid score"),
+    }
+}
+
+// Write a function that removes the spaces from the string, then return the resultant string.
+
+// Examples (Input -> Output):
+
+// "8 j 8   mBliB8g  imjB8B8  jl  B" -> "8j8mBliB8gimjB8B8jlB"
+// "8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd" -> "88Bifk8hB8BB8BBBB888chl8BhBfd"
+// "8aaaaa dddd r     " -> "8aaaaaddddr"
+pub fn no_space(x: String) -> String {
+    x.replace(" ", "")
+}
---
+++ src/main.rs
@@ -0,0 +1,13 @@
+mod kyu8;
+
+fn main() {
+    println!("Hello, world!");
+    println!("{}", kyu8::tasks::get_grade(82, 85, 87));
+    println!();
+    println!("=============================================");
+    println!();
+    println!(
+        "{}",
+        kyu8::tasks::no_space("8 j 8   mBliB8g  imjB8B8  jl  B".to_string())
+    );
+}
